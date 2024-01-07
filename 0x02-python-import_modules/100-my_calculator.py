e925d43ee25c8f260b19582967475ee80a641810
John Oduya <oduyajohn66@gmail.com>
2024-01-07 15:24:03 +0300

    100-calculater

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, div, mul, sub
-
-
-def main():
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    if argv[2] == '+':
-        print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
-    elif argv[2] == '-':
-        print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
-    elif argv[2] == '*':
-        print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
-    elif argv[2] == '/':
-        print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
 
 if __name__ == "__main__":
-    main()
+    """update the 1-calculator to receive CLD"""
+    import sys
+    from calculator_1 import add, mul, sub, div
 
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    signs = ["*", "+", "-", "/"]
+    argv = sys.argv
+    x = argv[1]
+    y = argv[2]
+    z = argv[3]
+    if y not in signs:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    else:
+        if argv[2] == '+':
+            print("{} {} {} = {}".format(x, y, z, add(int(x), int(z))))
+        elif argv[2] == "*":
+            print("{} {} {} = {}".format(x, y, z, mul(int(x), int(z))))
+        elif argv[2] == '/':
+            print("{} {} {} = {}".format(x, y, z, div(int(x), int(z))))
+        elif argv[2] == '-':
+            print("{} {} {} = {}".format(x, y, z, sub(int(x), int(z))))

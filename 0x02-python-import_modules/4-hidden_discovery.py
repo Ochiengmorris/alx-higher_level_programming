2c87e18150eefda17fdf4629ca8cd1a53504008f
John Oduya <oduyajohn66@gmail.com>
2024-01-07 15:03:51 +0300

    h-discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """print variable in hidden_4"""
-    import hidden_4
+    main()
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)i]))
+def main():
+    l = dir(hidden_4)
+    for i in range(len(l)):
+        if(l[i][0] != '_'):
+            print("{}".format(l[i]))

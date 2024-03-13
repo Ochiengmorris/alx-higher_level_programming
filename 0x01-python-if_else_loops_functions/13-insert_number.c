dbee84a76edb9ddcf594f4a83dbb501089d4b877
John Oduya <oduyajohn66@gmail.com>
2024-03-14 00:04:17 +0300

    11-power

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,34 +1,51 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_node - Inserts a number into a sorted singly linked list.
- * @head: A pointer to the head of the list.
- * @number: The number to insert.
- *
- * Return: The address of the new node, or NULL if it failed.
- */
+  * insert_node - Inserts a number into a sorted singly linked list
+  * @head: The head of the sorted singly linked list
+  * @number: The number to inserts in the singly linked list
+  *
+  * Return: The singly linked list with the new number added
+  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current = *head;
+	listint_t *current = NULL, *new_node = NULL, *tmp = NULL;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return NULL;
-    new_node->n = number;
-    new_node->next = NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (*head == NULL || number < (*head)->n)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
+	new_node->n = number;
+	if (*head)
+	{
+		current = *head;
+		if (number <= current->n)
+		{
+			new_node->next = current;
+			*head = new_node;
+		}
+		else
+		{
+			while (current->next)
+			{
+				if (number <= current->next->n)
+				{
+					tmp = current->next;
+					current->next = new_node;
+					new_node->next = tmp;
+					return (*head);
+				}
 
-    while (current && current->next && number >= current->next->n)
-        current = current->next;
-
-    new_node->next = current->next;
-    current->next = new_node;
-
-    return new_node;
+				current = current->next;
+			}
+			tmp = current->next;
+			current->next = new_node;
+			new_node->next = tmp;
+		}
+		return (*head);
+	}
+	new_node->next = NULL;
+	*head = new_node;
+	return (*head);
 }
